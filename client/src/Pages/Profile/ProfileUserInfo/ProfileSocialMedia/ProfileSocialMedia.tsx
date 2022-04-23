398c59d42a92434698598ab85d11421f331cd125
doog12 <drannikov.kirill@gmail.com>
2022-04-23 20:43:49 +0300

    Соц. сети на странице профиля

---
+++ client/src/Pages/Profile/ProfileUserInfo/ProfileSocialMedia/ProfileSocialMedia.tsx
@@ -0,0 +1,85 @@
+import React from 'react'
+
+import FacebookIcon from '../../../../assets/images/Profile/social-media-icons/facebook-icon.svg'
+import LinkedInIcon from '../../../../assets/images/Profile/social-media-icons/linkedin-icon.svg'
+import TelegramIcon from '../../../../assets/images/Profile/social-media-icons/telegram-icon.svg'
+import ViberIcon from '../../../../assets/images/Profile/social-media-icons/viber-icon.svg'
+import WhatsAppIcon from '../../../../assets/images/Profile/social-media-icons/whatsapp-icon.svg'
+import MailIcon from '../../../../assets/images/Profile/social-media-icons/mail-icon.svg'
+
+import './ProfileSocialMedia.scss'
+import { SocialMedia } from '../../../../models/response/UserInfoResponse'
+
+interface ProfileSocialMediaProps {
+    socialMedias: SocialMedia,
+    email: string
+}
+
+interface TypeOfSocialMediaResponse {
+    link: string,
+    icon: string
+}
+
+function typeOfSocialMedia(name: string, value?: string): TypeOfSocialMediaResponse {
+    if (name === 'telegram') {
+        return {
+            link: `https://t.me/${value}`,
+            icon: TelegramIcon
+        }
+    }
+    if (name === 'facebook') {
+        return {
+            link: `https://facebook.com/${value}`,
+            icon: FacebookIcon
+        }
+    }
+    if (name === 'linkedIn') {
+        return {
+            link: `https://www.linkedin.com/in/${value}`,
+            icon: LinkedInIcon
+        }
+    }
+    if (name === 'viber') {
+        return {
+            link: `viber://chat?number=${value}`,
+            icon: ViberIcon
+        }
+    }
+    if (name === 'whatsApp') {
+        return {
+            link: `https://wa.me/${value}`,
+            icon: WhatsAppIcon
+        }
+    }
+
+    if (name === 'email') {
+        return {
+            link: `mailto:${value}`,
+            icon: MailIcon
+        }
+    }
+
+    return {} as TypeOfSocialMediaResponse
+}
+
+const ProfileSocialMedia = ({socialMedias, email}: ProfileSocialMediaProps) => {
+
+    return (
+        <div className="profile-page__user__social-media">
+
+            <a href={typeOfSocialMedia('email', email).link}>
+                <img src={typeOfSocialMedia('email', email).icon} className="profile-page__user__social-media__item" alt='email' />
+            </a>
+
+            {
+                Object.entries(socialMedias).map(([key, value]: Array<string>, index: number) => (
+                    <a href={typeOfSocialMedia(key, value).link} target='_blank' rel="noreferrer" key={`${value}_${index}`}>
+                        <img src={typeOfSocialMedia(key).icon} className="profile-page__user__social-media__item" alt={key} />
+                    </a>
+                ))
+            }
+        </div>
+    )
+}
+
+export default ProfileSocialMedia
