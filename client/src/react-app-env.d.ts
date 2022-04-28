7f2cfc74b9fc55a8481f043c70505d8abceada56
doog12 <drannikov.kirill@gmail.com>
2022-04-28 13:48:27 +0300

    Update react-app-env.d.ts

--- client/src/react-app-env.d.ts
+++ client/src/react-app-env.d.ts
@@ -1,13 +1,26 @@
-// / <reference types="react-scripts" />
-declare global {
-    namespace NodeJS {
-        interface ProcessEnv {
-            TMDB_API: string
-        }
-    }
+declare module '*.svg?inline' {
+    const content: any
+    export default content
 }
 
-declare module '*.png'
-declare module '*.svg'
-declare module '*.jpeg'
-declare module '*.jpg'
+declare module '*.svg' {
+    const content: any
+    export default content
+}
+
+declare module '*.png' {
+    const content: any
+    export default content
+
+
+}
+
+declare module '*.jpg' {
+    const content: any
+    export default content
+}
+
+declare module '*.jpeg' {
+    const content: any
+    export default content
+}
